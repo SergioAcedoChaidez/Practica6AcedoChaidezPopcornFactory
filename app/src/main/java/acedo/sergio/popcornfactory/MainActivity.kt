b6f49970e3aab9b2b8765e9248b4a1fd9cf37e4f
SergioAcedoChaidez <sergio.acedo188999@potros.itson.edu.mx>
2021-10-07 15:22:20 -0700

    Projecto 6 Sergio Acedo Chaidez

---
+++ app/src/main/java/acedo/sergio/popcornfactory/MainActivity.kt
@@ -0,0 +1,32 @@
+package acedo.sergio.popcornfactory
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_detalle_pelicula.view.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.pelicula.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        botonP.setOnClickListener(){
+            var intent: Intent= Intent(this,catalogo::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+
+}
+
---
+++ app/src/main/java/acedo/sergio/popcornfactory/Pelicula.kt
@@ -0,0 +1,8 @@
+package acedo.sergio.popcornfactory
+
+data class Pelicula (var titulo: String,
+                     var image: Int,
+                     var header:Int ,
+                     var sipnosis :String)
+
+
